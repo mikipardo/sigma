4b16075b1113a27b36996a5abb019cad026f1bf0
Miki Pardo <92552184+mikipardo@users.noreply.github.com>
2023-10-06 15:02:26 +0200

    Update ValueChangeHtml.java

--- DriverSelenium/DriverSelenium/src/ValueChangeHtml.java
+++ DriverSelenium/DriverSelenium/src/ValueChangeHtml.java
@@ -1,1 +1,4 @@
 
+WebElement element = driver.findElement(By.id("XpahRuta"));
+JavascriptExecutor jse =(JavascriptExecutor)driver;
+jse.executeScript("arguments[0].value=\'"+variableImporte+"\';",element);
